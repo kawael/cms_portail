9f7a5a8a03b36e87a571376ffa08f6fc1cd79778
C.Wail <kawael09@gmail.com>
2023-12-07 21:53:14 +0000

    worked api

--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 from cms.views import api
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api\/', api.urls,name="api"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
